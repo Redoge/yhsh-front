41ab3ddec05ef59b14b40520a6bd034350794f69
Redoge <mail.redoge@gmail.com>
2023-06-24 21:41:51 +0300

    fixed some bugs

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {JwtService} from "../../service/jwt/jwt.service";
+import {UserRole} from "../../entity/UserRole";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
+
+  constructor(private router: Router, private jwtService: JwtService) {
+  }
+
+  ngOnInit(): void {
+    const role = this.jwtService.getUserRole();
+    if (role === UserRole.USER) {
+      this.router.navigate(['/user']);
+    } else if (role === UserRole.ADMIN){
+      this.router.navigate(['/admin']);
+    } else if (role === UserRole.ANONYMOUS){
+      this.router.navigate(['/login']);
+    }
+  }
 }
